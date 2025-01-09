124b88370a0400dffb8799ecade69a87135820f9
VasuSri <vasusrivastav1@gmail.com>
2025-01-09 18:12:56 +0530

    middleware fixed 100.0

--- src/routes/dashboard.route.js
+++ src/routes/dashboard.route.js
@@ -6,7 +6,7 @@ import isAdmin from '../middlewares/admin.middleware.js';
 const router = express.Router();
 
 router.get('/', authenticateJWT, getAllUsers);
-router.get('/:id', authenticateJWT,  getUserInfo);
 router.put('/:id/role', authenticateJWT, updateUserRole);
+router.get('/:id', authenticateJWT, getUserInfo);
 
 export default router;
