7a8f611f2ac73510f1f916c59da4707e7fc4efb0
VasuSri <vasusrivastav1@gmail.com>
2025-01-09 09:55:25 +0530

    cors header set

--- src/index.js
+++ src/index.js
@@ -20,8 +20,10 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.removeHeader('Cross-Origin-Opener-Policy');
-    res.removeHeader('Cross-Origin-Embedder-Policy');
+  // res.removeHeader('Cross-Origin-Opener-Policy');
+  // res.removeHeader('Cross-Origin-Embedder-Policy');
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   next();
 });
 // const allowedOrigins = [
